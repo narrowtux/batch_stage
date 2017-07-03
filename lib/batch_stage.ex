1e51cd880e10b0c376269d04f19a70d0edb73d6b
Moritz Schmale <narrow.m@gmail.com>
2017-07-03 10:42:15 +0200

    initial commit

---
+++ lib/batch_stage.ex
@@ -0,0 +1,18 @@
+defmodule BatchStage do
+  @moduledoc """
+  Documentation for BatchStage.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BatchStage.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/batch_stage_test.exs
@@ -0,0 +1,8 @@
+defmodule BatchStageTest do
+  use ExUnit.Case
+  doctest BatchStage
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
